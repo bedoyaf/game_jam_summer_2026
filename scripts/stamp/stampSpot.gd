abe0ac196d26b7218c43420b09f08f9de16be809
Maelie Bedoya <bedoyaf>
2026-04-25 01:32:19 +0200

    added walking scene

--- scripts/stamp/stampSpot.gd
+++ scripts/stamp/stampSpot.gd
@@ -1,25 +1,29 @@
-extends Node2D
+extends StaticBody2D
 
 @export var default_texture: Texture2D
 @export var stamped_texture: Texture2D
 
 var is_stamped: bool = false
+var disabled: bool = false
 
 @onready var sprite: Sprite2D = $Sprite2D
 
+@onready var collisionShape: CollisionShape2D = $CollisionShape2D
 
 func _ready() -> void:
 	_update_visual()
 
 
 func on_stamp():
-	if is_stamped:
+	if is_stamped or disabled:
 		return  # už je označený, ignoruj
 	
 	is_stamped = true
+	disabled = true
 	_update_visual()
 	
 	print("STAMPED!")  # debug nebo event
+	collisionShape.disabled = true
 
 
 func _update_visual():
---
+++ scripts/walking_around/decision_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var decision1 : StaticBody2D
+@export var decision2 : StaticBody2D 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(decision1.is_stamped):
+		decision2.disabled = true
+	elif(decision2.is_stamped):
+		decision1.disabled = true
